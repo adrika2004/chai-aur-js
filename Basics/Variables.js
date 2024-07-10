2835a6f0377c1d163af0f3a1fd698e7148f27e15
adrika2004 <adrikasaha.bln@gmail.com>
2024-07-10 07:41:57 +0000

    datatypes

---
+++ Basics/Variables.js
@@ -0,0 +1,12 @@
+const accountId=144563
+let accountEmail="adrika@gmail.com"
+var accountPassword="123"
+accountCity="Jaipur"
+
+//accountId=2    not allowed since it is const
+
+accountEmail="dfgaerger"
+accountPassword="12351"
+accountCity="Tripura"
+
+console.log([accountEmail,accountPassword,accountCity])
