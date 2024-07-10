81d86d2ffe817f9f9af77fa75c68634b2020439d
adrika2004 <adrikasaha.bln@gmail.com>
2024-07-10 09:14:07 +0000

    ConversionOperationcomparison

--- Basics/ConversionOperation.js
+++ Basics/ConversionOperation.js
@@ -1,9 +1,20 @@
-let score=33
+// let score=33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let isLoggedIn="Adrika"
+// let isLoggedIn="Adrika"
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*******************OPERATIONS******************* */
+
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2+2);
+// console.log(2%2);
+// console.log(2-2);
+
+ 
