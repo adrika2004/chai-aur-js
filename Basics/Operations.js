c2796c31cd4a366910e520f90b9e7359b634602a
adrika2004 <adrikasaha.bln@gmail.com>
2024-10-05 05:16:58 +0000

    Operations

---
+++ Basics/Operations.js
@@ -0,0 +1,8 @@
+let value1=3
+let value2=4
+// console.log(-value)
+console.log(value1+value2)
+console.log(value1-value2)
+console.log(value1*value2)
+console.log(value1/value2)
+console.log(value1%value2)
