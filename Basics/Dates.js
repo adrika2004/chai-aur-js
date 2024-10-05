210f6fca1a6e2e233004eb4b731dfe39a474d707
adrika2004 <adrikasaha.bln@gmail.com>
2024-10-05 09:58:27 +0000

    Dates

---
+++ Basics/Dates.js
@@ -0,0 +1,12 @@
+// let myDate= new Date()
+// let myCreatedDate=new Date(2023,0,23,5,6)
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toDateString());
+
+let Newdate=new Date();
+console.log(Newdate);
+console.log(Newdate.getMonth)
+Newdate.toLocaleString('default',{
+    weekday:"long",
+})
