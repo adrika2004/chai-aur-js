0ff0f455c5023a4d953b6d4eed7613ba4b454f1a
adrika2004 <adrikasaha.bln@gmail.com>
2024-10-05 10:28:54 +0000

    Arrays

---
+++ Basics/Arrays.js
@@ -0,0 +1,12 @@
+const myarr=[0,1,2,3,4];
+// console.log(arr[0]);
+//slice and Splice
+console.log("A",myarr);
+const myn1=myarr.splice(1,3)
+console.log(myn1)
+console.log("A",myarr)
+
+console.log("B",myarr);
+const myn2=myarr.slice(1,3)
+console.log(myn2)
+console.log("B",myarr)
